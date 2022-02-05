e59c343e4f7d205fb25aba1d2a13890aff81b0fc
Adam Garberg <adamgarberg@gmail.com>
2022-02-05 12:51:53 -0600

    input number functions finished

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,49 +2,87 @@ $(document).ready(onReady);
 console.log('Jquery is Hooked UP!');
 
 function onReady() {
-    $('#plus').on('click',null,'+', gatherInputs)
-    $('#negative').on('click',null,'-', gatherInputs)
-    $('#multiply').on('click',null,'*', gatherInputs)
-    $('#divide').on('click',null],'/', gatherInputs)
+    $('#plus').on('click', addInputs)
+    $('#negative').on('click', subtractInputs)
+    $('#multiply').on('click', multiplyInputs)
+    $('#divide').on('click', divideInputs)
     $('#clearButton').on('click', clearInputs)
+}
+//Clear inputs on button press
+function clearInputs() {
+    console.log('in clear inputs');
+    $('#firstInput').val('');
+    $('#secondInput').val('');
+}
+// start input functions
+function subtractInputs(){
+    console.log('in Subtract');
+    $('#equalsButton').on('click', subtract);
+}
+function subtract(){ 
+    let numInputs = [{
+    numOneInput:$('#firstInput').val(),
+    numTwoInput:$('#secondInput').val(),
+    operator: '-',
 
-    function clearInputs() {
-        console.log('in clear inputs');
-        $('#firstInput').val('');
-        $('#secondInput').val('');
-    }
-    function gatherInputs() {
-        $('#equalsButton').on('click', addInputs);{
-        console.log('in gather inputs');
-        }}
-
-    function addInputs(){
-        let numInputs = [{
-        operatorInput:'+',
-        numOneInput:$('#firstInput').val(),
-        numTwoInput:$('#secondInput').val(),
-        }]
-        console.log(numInputs);
-        
-    }
-        
-        
+    }]
     
+}    
 
-    // //POST data from inputs to server
-    // $.ajax({
-    //     method: 'POST',
-    //     url: '/calculator',
-    //     data: {
-    //     }
-    // }).then(function(){
-    //     console.log('guesses added successfully');
-    //     getGuessList();
-    // }).catch(function(response){
-    //   console.log('It did not work :(', response);
-    // })
-    // // getGuessList();
+function addInputs(){
+    console.log('in add');
+    $('#equalsButton').on('click', add)
+}
+
+function add(){
+    console.log('in add');
+    let numInputs = [{
+    numOneInput:$('#firstInput').val(),
+    numTwoInput:$('#secondInput').val(),
+    operator: '+',
+    }]
+    console.log(numInputs);
+}
+function multiplyInputs(){
+    console.log('in multiply');
+    $('#equalsButton').on('click',multiply)
+}
+function multiply(){ 
+    let numInputs = [{
+    numOneInput:$('#firstInput').val(),
+    numTwoInput:$('#secondInput').val(),
+    operator: '*'
+    }]
+    console.log(numInputs);
+}
     
+function divideInputs(){
+    console.log('in divide');
+    $('#equalsButton').on('click')
+}
+function divide(){ 
+    let numInputs = [{
+    numOneInput:$('#firstInput').val(),
+    numTwoInput:$('#secondInput').val(),
+    operator: '/'
+    }]
+    console.log(numInputs);
+}    
+// END input functions
+
+function getNumbers() {
+    // get numbersInputted from server using AJAX
+    $.ajax({
+      method: 'GET',
+      url: '/numbers'
+    }).then(function(response) {
+      console.log('We Got It!', response);
+      // TODO append quotes to DOM
+      renderToDom(response);
+    }).catch(function(response) {
+      console.log('Something Bad Happened, GET', response);
+    })
+  }
     
 
 
