47d6a5018e8eb356d579afde8246daa306a4f98a
Adam Garberg <adamgarberg@gmail.com>
2022-02-05 10:43:58 -0600

    clear inputs funtion operational

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,53 @@ $(document).ready(onReady);
 console.log('Jquery is Hooked UP!');
 
 function onReady() {
+    $('#plus').on('click',null,'+', gatherInputs)
+    $('#negative').on('click',null,'-', gatherInputs)
+    $('#multiply').on('click',null,'*', gatherInputs)
+    $('#divide').on('click',null],'/', gatherInputs)
+    $('#clearButton').on('click', clearInputs)
+
+    function clearInputs() {
+        console.log('in clear inputs');
+        $('#firstInput').val('');
+        $('#secondInput').val('');
+    }
+    function gatherInputs() {
+        $('#equalsButton').on('click', addInputs);{
+        console.log('in gather inputs');
+        }}
+
+    function addInputs(){
+        let numInputs = [{
+        operatorInput:'+',
+        numOneInput:$('#firstInput').val(),
+        numTwoInput:$('#secondInput').val(),
+        }]
+        console.log(numInputs);
+        
+    }
+        
+        
+    
+
+    // //POST data from inputs to server
+    // $.ajax({
+    //     method: 'POST',
+    //     url: '/calculator',
+    //     data: {
+    //     }
+    // }).then(function(){
+    //     console.log('guesses added successfully');
+    //     getGuessList();
+    // }).catch(function(response){
+    //   console.log('It did not work :(', response);
+    // })
+    // // getGuessList();
+    
+    
+
+
+    
 
 
 
@@ -24,10 +71,3 @@ function onReady() {
 
 
 
-
-
-
-
-
-
-}
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
-
-
 app.use(express.static('server/public'));
+const { post } = require('jquery');
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+
 
 
 
